5fba7e9eb261709983d0b7159e95eba882f5f5b5
Vignesh Dhanasekaran <v.dhanasekaran0@gmail.com>
2018-09-29 20:14:39 -0400

    Updating analysis

--- simulation/analyzer.py
+++ simulation/analyzer.py
@@ -10,6 +10,7 @@ import json
 import os
 
 import matplotlib.pyplot as plt
+import matplotlib.cm as cm
 import numpy
 from collections import Counter, defaultdict
 
@@ -53,13 +54,14 @@ def get_point_data(parameters, raw_data):
     years = [subdct["Years"] for key, subdct in raw_data.items()]
     mean_years = numpy.mean(years)
     median_years = numpy.median(years)
-
+    std_years = numpy.std(years)
 
     return {
         "x" : medium_layout,
         "y" : large_layout,
         "mean_years" : mean_years,
-        "median_years" : median_years
+        "median_years" : median_years,
+        "std_years" : std_years
     }
 
 
@@ -102,7 +104,12 @@ if __name__ == '__main__':
     param_x = []
     param_y = []
     param_c = []
+    param_std = []
 
+    color_array = numpy.zeros(shape=(8, 7))
+    std_array = numpy.empty(shape=(8, 7), dtype=object)
+
+    color_dict = {}
 
     for result in results:
 
@@ -115,16 +122,41 @@ if __name__ == '__main__':
         param_x.append(test["x"])
         param_y.append(test["y"])
         param_c.append(test["mean_years"])
+        param_std.append(test["std_years"])
 
+        color_array[int((test["y"]-7)/-2)][int((test["x"]+6)/2)] = test["mean_years"]
+        std_array[int((test["y"] - 7) / -2)][int((test["x"] + 6) / 2)] = \
+            "{0:0.2f} ± {1:0.2f}".format(test["mean_years"], test["std_years"])
 
     # Divide each element by the max
     # We do the 1 -, because we prefer lower years to higher ones
     # param_c = [1 - x / max(param_c) for x in param_c]
+
     plt.xticks(numpy.arange(min(param_x), max(param_x) + 1, 2.0))
     plt.yticks(numpy.arange(min(param_y), max(param_y) + 1, 2.0))
 
-    sc = plt.scatter(param_x, param_y, c=param_c,  s=100, cmap='YlGn_r', edgecolors='black', linewidths=.5)
+    sc = plt.scatter(param_x, param_y, c=param_c,  s=500, marker='s', cmap='YlGn_r', edgecolors='black', linewidths=.5)
     plt.colorbar(sc)
+
+
+
+
+    color_array[7][0] = max(param_c)
+    color_array[0][6] = max(param_c)
+    fig, ax = plt.subplots()
+    i = ax.imshow(color_array, cmap='YlGn_r', interpolation='nearest')
+    fig.colorbar(i)
+
+    fig.savefig("heat_map.eps", format="eps", dpi=1000)
+
+    print(param_x)
+    print(param_y)
+    print(param_c)
+    print(param_std)
+    print()
+    print(color_array)
+    print(std_array)
+
 
     # plt.legend(loc='upper left')
     plt.show()
